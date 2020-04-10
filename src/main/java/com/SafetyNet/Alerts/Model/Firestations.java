8b03e5b9ea7dc96f63859d4351a2fe8202c59dd8
Hossam-C <chenaoui.hossam@gmail.com>
2020-04-10 14:29:06 +0200

    First commit : - project initialization - first endpoint : person

---
+++ src/main/java/com/SafetyNet/Alerts/Model/Firestations.java
@@ -0,0 +1,24 @@
+package com.SafetyNet.Alerts.Model;
+
+public class Firestations {
+
+    private String address;
+    private int station;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStation() {
+        return station;
+    }
+
+    public void setStation(int station) {
+        this.station = station;
+    }
+
+}
---
+++ src/test/java/com/SafetyNet/Alerts/readJsonTest.java
@@ -0,0 +1,31 @@
+package com.SafetyNet.Alerts;
+
+
+import com.SafetyNet.Alerts.Model.Firestations;
+import com.SafetyNet.Alerts.Model.Medicalrecords;
+import com.SafetyNet.Alerts.Model.Persons;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+public class readJsonTest {
+
+//    private LoadJson loadJson;
+
+    @Test
+    public void testlectureJson() throws IOException {
+
+/*        loadJson = new LoadJson();
+        ReadJson readJson = new ReadJson();
+
+        ArrayList<Persons> lpersons = loadJson.readPersons(readJson.read());
+        ArrayList<Firestations> lfirestations = loadJson.readFirestations(readJson.read());
+        ArrayList<Medicalrecords> lmedicalRecords = loadJson.readMedicalRecord(readJson.read());*/
+
+        System.out.println("Ok");
+
+    }
+}
